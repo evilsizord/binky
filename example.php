04e49698c614440bf5127aa0ca171c993aec4a7d
Daniel Evilsizor <evilsizord@gmail.comm>
2019-05-25 10:09:21 -0500

    add code

---
+++ example.php
@@ -0,0 +1,228 @@
+<?php
+
+require "pharse/pharse.php";
+require "binky.php";
+
+$b = new Binky();
+
+$b->addComponent('p.ranking', '<p style="padding: 0; margin: 5px 0; font-family: arial,sans-serif; font-size: 14px; line-height: 18px; font-weight: bold;">{{INNERTEXT}}</p>');
+$b->addComponent('a.ranking', '<span style="font-size: 12px;"><a style="text-decoration: underline; color: #00447c;" target="_blank" {{=HREF}} >{{INNERTEXT}}</a></span>');
+
+$b->startBuffering();
+
+/************** YOUR EMAIL HERE **********************************/   ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
+  <meta content="en-us" http-equiv="Content-Language" />
+  <title>UTA 2018-2019 Highlights</title>
+</head>
+
+<body style="padding:0;margin:0;background-color:#00447c;color:#00447c;">
+	
+	<preheader>Read all about this amazing great email</preheader>
+	
+	<container width="100%" background="https://www.uta.edu/enews/email/2019/rankings/_img/pattern.gif" bgcolor="#00447c">
+    
+    <!--[if gte mso 9]>
+    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
+        <v:fill type="tile" src="https://www.uta.edu/enews/email/2019/rankings/_img/pattern.gif" color="#00447c" />
+        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
+    <![endif]-->
+	
+	<container width="600">
+	
+		<row>
+			<column>
+				<p
+					style="padding: 0; margin: 10px 0; font-family: arial,sans-serif; font-size: 12px; text-align: center; line-height: 24px;">
+					<a style="color: #57B6FF; text-decoration: none;"
+					href="https://www.uta.edu/enews/email/2019/rankings/email.html">View in browser</a>
+				</p>
+			</column>
+		</row>
+		
+		<row>
+			<column>
+				<a href="https://www.uta.edu" style="text-decoration: none; color: #ffffff;">
+					<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/uta-logo-1.jpg" alt="The University of Texas at Arlington" width="600" height="63"/>
+				</a>
+			</column>
+		</row>
+		
+		<row>
+			<column>
+				<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/hero-1.jpg"  width="600" height="215" alt="Access. Excellence. Success."/>
+			</column>
+		</row>
+		
+		<row>
+			<column>
+				<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/shadow.png"  width="600" height="15"/>
+			</column>
+		</row>
+		
+        <row width="520" align="center">
+			<column padding="15px 0 30px 0" textalign="center">
+				<p style="padding: 0; margin: 0; font-family: arial, sans-serif; font-size: 14px; line-height: 18px; color: #ffffff; text-align:center;">
+					The University of Texas at Arlington is a comprehensive urban-serving research institution serving more than 60,000 degree-seeking students.
+				</p>
+			</column>
+		</row>
+		
+		<container width="520" bgcolor="#ffffff" style="color: #00447c; text-align: center; border: 2px solid #f58025;">
+		
+			<row>
+				<column textalign="center" padding="20px 30px 5px 30px">
+					<p style="padding: 0; margin: 5px 0; font-family: arial,sans-serif; font-size: 23px; line-height: 23px; font-weight: bold; color:#f58025;">
+						Select Highlights for the 2018-19 academic year
+					</p>
+				</column>
+			</row>
+			
+			<row>
+				<column textalign="center" padding="10px 30px 5px 30px">
+					<p class="ranking">
+						History professor named a 2019 Carnegie Fellow<br />
+						<a class="ranking" href="https://www.carnegie.org/awards/andrew-carnegie-fellows/2019/">Carnegie Corporation of New York</a>
+					</p>
+				</column>
+			</row>
+			
+			<row width="455" align="center">
+				<column>
+					<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/list-border.png"  width="455" height="3"/>
+				</column>
+			</row>
+			
+			<row>
+				<column textalign="center" padding="5px 30px 5px 30px">
+					<p class="ranking">
+						MAE Professor elected to the National Academy of Engineering<br />
+						<a class="ranking" href="https://www.nae.edu/204037.aspx">NAE</a>
+					</p>
+				</column>
+			</row>
+			
+			<row width="455" align="center">
+				<column>
+					<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/list-border.png"  width="455" height="3"/>
+				</column>
+			</row>
+			
+			<row>
+				<column textalign="center" padding="5px 30px 5px 30px">
+					<p class="ranking">
+						MAE Professor elected to the National Academy of Engineering<br />
+						<a class="ranking" href="https://www.nae.edu/204037.aspx">NAE</a>
+					</p>
+				</column>
+			</row>
+			
+			<row width="455" align="center">
+				<column>
+					<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/list-border.png"  width="455" height="3"/>
+				</column>
+			</row>
+			
+			<row>
+				<column textalign="center" padding="5px 30px 5px 30px">
+					<p class="ranking">
+						MAE Professor elected to the National Academy of Engineering<br />
+						<a class="ranking" href="https://www.nae.edu/204037.aspx">NAE</a>
+					</p>
+				</column>
+			</row>
+			
+			<row width="455" align="center">
+				<column>
+					<img class="default" src="https://www.uta.edu/enews/email/2019/rankings/_img/list-border.png"  width="455" height="3"/>
+				</column>
+			</row>
+			
+		</container>
+		
+		<row width="425" align="center">
+			<column padding="20px 0 20px 0;">
+				<a href="https://www.uta.edu" style="text-decoration: none; color: #ffffff;">
+					<img border=”0″ style="border:none; display:block;  margin: 0 auto;" src="https://www.uta.edu/enews/email/2019/rankings/_img/uta-logo-horizontal.png" alt="The University of Texas at Arlington" width="425" height="40"/>
+				</a>
+			</column>
+		</row>
+		
+		<row align="center" width="520" style="border-top: 1px solid #fff;">
+		
+			<column width="320" padding="20px 0 20px 0">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 16px; font-weight: bold; line-height: 5px;">
+					Get Connected
+				</p>
+			</column>
+			<column width="50" padding="20px 0 20px 0">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 10px; line-height: 5px;">
+					<a href="https://www.facebook.com/utarlington/" style="color:#ffffff; text-decoration:none;">
+						<img alt="Facebook" class="default" height="30" src="https://www.uta.edu/enews/_img/_elements/social-circle-facebook.png" width="30" />
+					</a>
+				</p>
+			</column>
+			<column width="50" padding="20px 0 20px 0;">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 10px; line-height: 5px;">
+					<a href="https://twitter.com/utarlington" style="color:#ffffff; text-decoration:none;">
+						<img alt="Twitter" border="0" height="30" hspace="0" src="https://www.uta.edu/enews/_img/_elements/social-circle-twitter.png" style="display: block;" vspace="0" width="30">
+					</a>
+				</p>
+			</column>
+			<column width="50" padding="20px 0 20px 0;">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 10px; line-height: 5px;">
+					<a href="https://www.linkedin.com/edu/school?id=19517" style="color:#ffffff; text-decoration:none;">
+						<img alt="LinkedIn" border="0" height="30" hspace="0" src="https://www.uta.edu/enews/_img/_elements/social-circle-linkedin.png" style="display: block;" vspace="0" width="30">
+					</a>
+				</p>
+			</column>
+			<column width="50" padding="20px 0 20px 0;">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 10px; line-height: 5px;">
+					<a href="https://www.instagram.com/utarlington/" style="color:#ffffff; text-decoration:none;">
+						<img alt="Instagram" border="0" height="30" hspace="0" src="https://www.uta.edu/enews/_img/_elements/social-circle-instagram.png" style="display: block;" vspace="0" width="30">
+					</a>
+				</p>
+			</column>
+		</row>
+		
+	</container>
+
+	<!-- FOOTER -->
+	<container width="100%" bgcolor="#0064b1">
+	
+		<row width="600" align="center" >
+			<column padding="25px 0 25px 0">
+				<p style="padding: 0; margin: 5px 0; color: #fff; font-family: arial,sans-serif; font-size: 13px; line-height: 21px; text-align: center;">
+					<a style="text-decoration: underline; color: #fff;" href="https://www.uta.edu/president/"><strong>Office of the President</strong></a>
+					• 701 S. Nedderman Dr. • Arlington, Texas 76019 • 817-272-2101<br/>
+					© 2019 The University of Texas at Arlington<br/>
+					<a style="color:#ffffff; text-decoration:underline;" href="https://www.uta.edu/uta/unsubscribe.php?product=all">Unsubscribe</a>
+				</p>
+			</column>
+		</row>
+		
+	</container>
+
+<!--[if gte mso 9]>
+  </v:textbox>
+</v:rect>
+<![endif]-->
+
+</container>
+
+<!-- Google analytics pixel -->
+<gapixel ua="297365-15" cn="Peer Letter" cs="Batch1"/>
+
+</body>
+</html>
+
+<?php  /************** END EMAIL **********************************/
+
+$b->stopBuffering();
+$b->render();
+
